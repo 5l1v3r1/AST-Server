ae6ea975a68b28d216d3bda42b26fe320ac00338
Sad Pony <51142088+sadponyguerillaboy@users.noreply.github.com>
2019-10-22 18:32:33 -0400

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,43 +2,43 @@
 # Uninstaller for removing previous installations of Apple Service Toolkit (not for uninstalling this server)
 
 # Unload LaunchDaemons
-launchctl unload /System/Library/LaunchDaemons/com.apple.gw_controld.plist
-launchctl unload /System/Library/LaunchDaemons/com.apple.gw_datad.plist
-launchctl unload /System/Library/LaunchDaemons/com.apple.gw_logd.plist
+launchctl unload /Library/LaunchDaemons/com.apple.gw_controld.plist
+launchctl unload /Library/LaunchDaemons/com.apple.gw_datad.plist
+launchctl unload /Library/LaunchDaemons/com.apple.gw_logd.plist
 
 # Remove Keychain Items
-rm /System/Library/Keychains/AppleServiceToolkit.keychain
+rm /Library/Keychains/AppleServiceToolkit.keychain
 
 # Remove Launch Daemons
-rm /System/Library/LaunchDaemons/com.apple.gw_controld.plist
-rm /System/Library/LaunchDaemons/com.apple.gw_datad.plist
-rm /System/Library/LaunchDaemons/com.apple.gw_logd.plist
+rm /Library/LaunchDaemons/com.apple.gw_controld.plist
+rm /Library/LaunchDaemons/com.apple.gw_datad.plist
+rm /Library/LaunchDaemons/com.apple.gw_logd.plist
 
 # Remove Preferences
 rm /Library/Preferences/com.apple.gateway.plist
 rm ~/Library/Preferences/com.apple.GatewayManager.plist
 
 # Remove command line utilities
-rm /usr/bin/gw_controld
-rm /usr/bin/gw_datad
-rm /usr/bin/gw_logd
+rm /usr/local/libexec/gw_controld
+rm /usr/local/libexec/gw_datad
+rm /usr/local/libexec/gw_logd
 
 # Remove Diagnostics Gateway & support files
 rm -rf /Applications/Gateway\ Manager.app
 rm -rf /private/var/adg
 
 # Remove Netboot Sets
-rm -rf /Library/NetBoot/NetBootSP0/AppleServiceToolkit.nbi
-rm -rf /Library/NetBoot/NetBootSP0/AppleDiagnosticOSs
-rm -rf /Library/NetBoot/NetBootSP0/AppleDiagnosticOS
-rm -rf /Library/NetBoot/NetBootSP0/AST64bit.nbi
-rm -rf /Library/NetBoot/NetBootSP0/ASTLegacy.nbi
-rm -rf /Library/NetBoot/NetBootSP0/Diagnostics.nbi
-rm -rf /Library/NetBoot/NetBootSP0/i386
-rm -rf /Library/NetBoot/NetBootSP0/TAOS_*
+#rm -rf /Library/NetBoot/NetBootSP0/AppleServiceToolkit.nbi
+#rm -rf /Library/NetBoot/NetBootSP0/AppleDiagnosticOSs
+#rm -rf /Library/NetBoot/NetBootSP0/AppleDiagnosticOS
+#rm -rf /Library/NetBoot/NetBootSP0/AST64bit.nbi
+#rm -rf /Library/NetBoot/NetBootSP0/ASTLegacy.nbi
+#rm -rf /Library/NetBoot/NetBootSP0/Diagnostics.nbi
+#rm -rf /Library/NetBoot/NetBootSP0/i386
+#rm -rf /Library/NetBoot/NetBootSP0/TAOS_*
 
 # Remove receipts
-rm /Library/Receipts/apple.com.astos*
+#rm /Library/Receipts/apple.com.astos*
 
 # Remove log files
 rm -rf /private/var/log/adg
